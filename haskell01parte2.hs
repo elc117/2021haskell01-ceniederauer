16f13a40fa6f812bbc604dac9a081f190b337398
Carlos Eduardo Niederauer Rodrigues <>
2021-05-26 17:21:31 +0000

    Falta a 9 da parte 2 e a parte 3

--- haskell01parte2.hs
+++ haskell01parte2.hs
@@ -1,2 +1,58 @@
 -- Prática 01 de Haskell - Parte 2
 -- Nome: Carlos Eduardo Niederauer Rodrigues
+
+
+-- Importando funcoes da parte 1 --
+htmlItem :: String -> String
+htmlItem item = "<li>" ++ item ++ "</li>"
+
+isVowel2 :: Char -> Bool
+isVowel2 char = elem char "aeiouAEIOU"
+
+isElderly :: Int -> Bool
+isElderly idade = idade > 65
+
+isLongWord :: String -> Bool 
+isLongWord s = length s > 10
+
+isEvenBetter  :: Int -> Bool
+isEvenBetter n = mod n 2 == 0
+
+circleArea :: Float -> Float
+circleArea r = r^2 * pi 
+--
+
+-- Funcoes auxiliares -- 
+between60and80 :: Int -> Bool
+between60and80 lis = lis >= 60 && lis <= 80
+
+isSpace :: Char -> Bool
+isSpace space = space == ' '
+--
+
+itemize :: [String] -> [String]
+itemize lis = map htmlItem lis
+
+onlyVowels :: String -> String
+onlyVowels string = filter isVowel2 string 
+
+onlyElderly :: [Int] -> [Int]
+onlyElderly age = filter isElderly age
+
+onlyLongWords :: [String] -> [String]
+onlyLongWords string = filter isLongWord string
+
+onlyEven :: [Int] -> [Int]
+onlyEven lis = filter isEvenBetter lis
+
+onlyBetween60and80 :: [Int] -> [Int]
+onlyBetween60and80 lis = filter between60and80 lis 
+
+countSpaces :: String -> Int
+countSpaces count = length (filter isSpace count)
+
+calcAreas :: [Float] -> [Float]
+calcAreas lis = map circleArea lis 
+
+charFound :: Char -> String -> Bool
+charFound c = 
